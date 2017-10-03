73511a9bfef86849b41bd1f9010aa14191248781
zumpf <niklas.zumpf@accenture.com>
2017-10-03 10:48:40 +0200

    Updated Text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Automated Build!");
+		System.out.println("Hello Automated Build! Test");
 	}
 
 }
